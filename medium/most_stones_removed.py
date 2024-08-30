09ba84e7ca607682277ef954a66364850d89169c
temesgen5335 <temesgen5335@gmail.com>
2024-08-30 17:21:16 +0300

    bew challenge most stones removed

---
+++ medium/most_stones_removed.py
@@ -0,0 +1,69 @@
+"""
+*** Problem Identification ***
+
+On a 2D plane, we place n stones at some integer coordinate points. Each coordinate point may have at most one stone.
+
+A stone can be removed if it shares either the same row or the same column as another stone that has not been removed.
+
+Given an array stones of length n where stones[i] = [xi, yi] represents the location of the ith stone, return the largest possible number of stones that can be removed.
+
+ 
+
+Example 1:
+
+Input: stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
+Output: 5
+Explanation: One way to remove 5 stones is as follows:
+1. Remove stone [2,2] because it shares the same row as [2,1].
+2. Remove stone [2,1] because it shares the same column as [0,1].
+3. Remove stone [1,2] because it shares the same row as [1,0].
+4. Remove stone [1,0] because it shares the same column as [0,0].
+5. Remove stone [0,1] because it shares the same row as [0,0].
+Stone [0,0] cannot be removed since it does not share a row/column with another stone still on the plane.
+Example 2:
+
+Input: stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]
+Output: 3
+Explanation: One way to make 3 moves is as follows:
+1. Remove stone [2,2] because it shares the same row as [2,0].
+2. Remove stone [2,0] because it shares the same column as [0,0].
+3. Remove stone [0,2] because it shares the same row as [0,0].
+Stones [0,0] and [1,1] cannot be removed since they do not share a row/column with another stone still on the plane.
+Example 3:
+
+Input: stones = [[0,0]]
+Output: 0
+Explanation: [0,0] is the only stone on the plane, so you cannot remove it.
+ 
+"""
+
+def removeStones(stones):
+    from collections import defaultdict
+
+    def dfs(node):
+        visited.add(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                dfs(neighbor)
+
+    # Create a graph where stones[i] is connected to stones[j] if they share a row or column
+    graph = defaultdict(list)
+    for i in range(len(stones)):
+        for j in range(i + 1, len(stones)):
+            if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]:
+                graph[i].append(j)
+                graph[j].append(i)
+
+    visited = set()
+    components = 0
+
+    for i in range(len(stones)):
+        if i not in visited:
+            dfs(i)
+            components += 1
+
+    return len(stones) - components
+
+# Example usage:
+stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
+print(removeStones(stones))  # Output: 5
