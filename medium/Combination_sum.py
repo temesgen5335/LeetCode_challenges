4ac0842bf7e6dd1a707f9eb6c7e51d6c7489f9e0
temesgen5335 <temesgen5335@gmail.com>
2024-09-26 13:41:49 +0300

    combination sum update

--- medium/Combination_sum.py
+++ medium/Combination_sum.py
@@ -2,7 +2,8 @@
 
 Combination Sum II
 
-Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+Given a collection of candidate numbers (candidates) and a target number (target),
+find all unique combinations in candidates where the candidate numbers sum to target.
 
 Each number in candidates may only be used once in the combination.
 
