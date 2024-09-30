d95ee60759f7c531e197cd0581f12452d6e31760
temesgen5335 <temesgen5335@gmail.com>
2024-09-30 22:51:06 +0300

    fibonacci number

--- easy/fibonacci_Number.py
+++ easy/fibonacci_Number.py
@@ -1,6 +1,7 @@
 
 """
 *** Problem Identification***
+
 The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
 
 F(0) = 0, F(1) = 1
@@ -26,6 +27,8 @@ Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 Constraints:
 
 0 <= n <= 30
+
+*** Solution ***
 """
 
 
